bb7b492aba1b9209e36912fb76b393a6448afd32
infinitebolas <rouinsardneven@gmail.com>
2026-01-30 10:21:34 +0100

    ajout de cors

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const mariadb = require('mariadb');
 const crypto = require('crypto');
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
 
 
