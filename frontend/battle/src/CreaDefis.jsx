db5a55f95701425c7eff5d66422a626a989a377f
infinitebolas <rouinsardneven@gmail.com>
2026-02-02 14:15:41 +0100

    front défi

--- frontend/battle/src/CreaDefis.jsx
+++ frontend/battle/src/CreaDefis.jsx
@@ -3,20 +3,31 @@ import { useState } from 'react';
 function Creation() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [difficulty, setDifficulty] = useState("EASY");
+
+  const resetForm = () => {
+    setTitle("");
+    setContent("");
+    setDifficulty("EASY");
+  };
+
   return (
-    <div className='defi'>
-      <form onSubmit={questionnaire}>
+    <div className="defi">
+      <form>
         <h3>Créer un défi</h3>
         <input type="text" placeholder="Titre du défi" value={title} onChange={(e) => setTitle(e.target.value)}/>
         <p>Choisir la difficulté</p>
-        <select name="difficulte">
-            <option>EASY </option>
-            <option>MEDIUM</option>
-            <option>HARD</option>
+        <select name="difficulte" value={difficulty} onChange={(e) => setDifficulty(e.target.value)}>
+          <option value="EASY">EASY</option>
+          <option value="MEDIUM">MEDIUM</option>
+          <option value="HARD">HARD</option>
         </select>
         <p>Définissez l'énoncé</p>
-        <textarea name="contenu" id="saisie" placeholder="Entrez votre défi" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
-        <button className="publier" type="submit">Publier</button>
+        <textarea name="contenu" id="saisie" placeholder="Entrez votre défi" value={content} onChange={(e) => setContent(e.target.value)}/>
+        <button type="button" onClick={resetForm}>
+          Réinitialiser
+        </button>
+        <Link to="/defis"><button className="publier" type="submit">Publier</button></Link>
       </form>
     </div>
   );
